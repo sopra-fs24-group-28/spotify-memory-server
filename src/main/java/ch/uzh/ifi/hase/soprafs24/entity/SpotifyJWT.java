6b0cf372b9b00807346620417a7c4f7097e95dad
NicSchuler <nicolas.schuler1998@gmail.com>
2024-04-04 14:02:59 +0200

    Add lombok to spotifyJWT

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/SpotifyJWT.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/SpotifyJWT.java
@@ -1,5 +1,9 @@
 package ch.uzh.ifi.hase.soprafs24.entity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class SpotifyJWT {
 
     private String accessToken;
@@ -11,45 +15,4 @@ public class SpotifyJWT {
     private String tokenType;
 
     private String scope;
-
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-
-    public Integer getExpiresln() {
-        return expiresln;
-    }
-
-    public void setExpiresln(Integer expiresln) {
-        this.expiresln = expiresln;
-    }
-
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public void setScope(String scope) {
-        this.scope = scope;
-    }
 }
