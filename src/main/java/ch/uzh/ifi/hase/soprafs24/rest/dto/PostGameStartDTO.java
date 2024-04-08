745485aad6efc1f08d67e6274bf1e9cd89143809
NicSchuler <nicolas.schuler1998@gmail.com>
2024-04-08 10:47:45 +0200

    minor correction so that gameId is returned to comply with rest spec

--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/PostGameStartDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/PostGameStartDTO.java
@@ -3,7 +3,7 @@ package ch.uzh.ifi.hase.soprafs24.rest.dto;
 import ch.uzh.ifi.hase.soprafs24.model.game.GameParameters;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record PostGameStartDTO(Integer gameId, @JsonProperty GameParameters gameParameters) {}
+public record PostGameStartDTO(@JsonProperty Integer gameId, @JsonProperty GameParameters gameParameters) {}
 
 
 
