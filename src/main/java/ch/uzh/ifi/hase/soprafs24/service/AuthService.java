200d061a25439f8998b806380fee0b1797d1f847
NicSchuler <nicolas.schuler1998@gmail.com>
2024-04-08 11:15:49 +0200

    remove unused line causing error

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/AuthService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/AuthService.java
@@ -1,7 +1,6 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
 
-import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs24.entity.SpotifyJWT;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
